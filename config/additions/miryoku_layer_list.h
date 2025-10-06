c1d55e5122611d804c0aeb0446efd230247b8258
Lukas Poorthuis <lukas.poorthuis@posteo.de>
2025-10-06 23:51:10 +0200

    refactor layer definitions and add layout mapping macros

--- config/additions/miryoku_layer_list.h
+++ config/additions/miryoku_layer_list.h
@@ -8,16 +8,23 @@
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun")
 
 #define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
+#define U_TAP    1
+#define U_BUTTON 2
+#define U_NAV    3
+#define U_MOUSE  4
+#define U_MEDIA  5
+#define U_NUM    6
+#define U_SYM    7
+#define U_FUN    8
 
 #endif
