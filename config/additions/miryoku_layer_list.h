ae0167bf3a19211c02e8a24afbdfb02d706c01d9
Lukas Poorthuis <lukas.poorthuis@posteo.de>
2025-10-06 23:02:52 +0200

    try dynamic layer mapping

---
+++ config/additions/miryoku_layer_list.h
@@ -0,0 +1,23 @@
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+// generated -*- buffer-read-only: t -*-
+
+#pragma once
+
+#if !defined (MIRYOKU_LAYER_LIST)
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+
+#endif
---
+++ config/urchin.h
@@ -0,0 +1,18 @@
+#define U_MACRO_VA_ARGS(macro, ...) macro(__VA_ARGS__)
+#define U_STRINGIFY(x) #x
+#define U_MACRO(name,...) \
+/ { \
+  macros { \
+    name: name { \
+      compatible = "zmk,behavior-macro"; \
+      #binding-cells = <0>; \
+      __VA_ARGS__ \
+    }; \
+  }; \
+};
+
+#define U_NP &none // key is not present
+#define U_NA &none // present but not available for use
+#define U_NU &none // available but not used
+
+#define U_TAPPING_TERM 200
